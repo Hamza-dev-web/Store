f170987cf1ab6066c6c6158d518f18172091a03f
unknown <upookluy@gmail.com>
2025-10-29 06:58:57 +0100

    Initial commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,15 +1,12 @@
-import ContextProvider from "@/context/context"
+
 import "../globals.css"
 import Image from "next/image"
 export default  function Layout({children} :{children : React.ReactNode}){
 return (
-<div className=" flex min-h-screen">
+<div className=" flex min-h-screen min-w-screen">
 <section >
-<ContextProvider>
 {children}
-</ContextProvider>
  </section>
-
     </div>
 )
 }
